696abeb1f8e32bae33da5b45ccfc7d71fd0ecfe9
Prince Carl Velasco <github@princevelasco.com>
2023-05-16 21:50:38 +0800

    init: Initial commit

---
+++ getStarted/getStarted/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-kx(egrsi&8p64(#k2tr101nc=x+$543$2rnhns1y(@_=&s^!7f"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
