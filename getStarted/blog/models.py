96806f3dfd41b061ab7e491963baa87617eea697
Prince Carl Velasco <github@princevelasco.com>
2023-05-16 23:39:50 +0800

    refactor: get only live pages ordered by first published at

--- getStarted/blog/models.py
+++ getStarted/blog/models.py
@@ -13,6 +13,12 @@ class BlogIndexPage(Page):
         FieldPanel('intro')
     ]
 
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        context['blogpages'] = self.get_children().live().order_by('-first_published_at')
+        return context
+
 
 class BlogPage(Page):
     date = models.DateField("Post date")
